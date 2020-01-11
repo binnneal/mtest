4145e8647d9f8b077dab21d6670edec140997053
Glitch (hello-webpage) <none>
2020-01-11 08:22:46 +0000

    💇🌑 Checkpoint ./memory/memory_test.js:10229510/10295

--- memory/memory_test.js
+++ memory/memory_test.js
@@ -31,7 +31,7 @@ function start_test() {
   }
   numbers_ele.innerHTML = [...numbers].join(" ");
   change_display(numbers_ele, true);
-  
+
   start_timer(SHOW_TIME, () => {
     change_display(numbers_ele, false);
     setTimeout(() => start_input(numbers), 0);
@@ -39,11 +39,14 @@ function start_test() {
 }
 
 function start_timer(timeout, exp_callback) {
+  change_display(timer_ele, true);
+
   timer_ele.innerHTML = `${timeout--} seconds remaining`;
   const timer_d = setInterval(() => {
     timer_ele.innerHTML = `${timeout--} seconds remaining`;
     if (timeout < 0) {
       clearInterval(timer_d);
+      change_display(timer_ele, false);
       setTimeout(exp_callback, 0);
     }
   }, 1000);
@@ -56,7 +59,7 @@ function start_input(numbers) {
     inputs = area_ele.value
       .split(/\s+/)
       .map(x => Number(x.trim()))
-      .filter(x => !isNaN(x));
+      .filter(x => x);
     change_display(area_ele, false);
     setTimeout(() => scoring(numbers, new Set(inputs)), 0);
   });
@@ -65,23 +68,32 @@ function start_input(numbers) {
 let results = [];
 
 function scoring(orig, input) {
-  const score = [...orig].reduce((accr, x) => input.has(x)?accr+1:accr, 0);
-  console.log(orig, input);
-  console.log(`score: ${score}`);
+  const score = [...orig].reduce(
+    (accr, x) => (input.has(x) ? accr + 1 : accr),
+    0
+  );
   results.push({
-    orig, input, score
+    orig,
+    input,
+    score
   });
   if (results.length < TEST_REPEAT) {
     setTimeout(() => {
-      button_ele.value = `Start Test ${results.length+1} of ${TEST_REPEAT}`;
+      button_ele.value = `Start Test ${results.length + 1} of ${TEST_REPEAT}`;
       change_display(button_ele, true);
     }, 0);
+  } else {
+    final_result();
   }
 }
 
+function final_result() {
+  document.getElementById('result').innerHTML = String(results);
+}
+
 function generate_test_set() {
-      button_ele.value = `Start Test ${results.length+1} of ${TEST_REPEAT}`;
-  button_ele.addEventListener('click', start_test);
+  button_ele.value = `Start Test ${results.length + 1} of ${TEST_REPEAT}`;
+  button_ele.addEventListener("click", start_test);
 }
 
 //scoring(new Set([11, 22, 33, 4, 2]), new Set([2, 56, 33]))
