57e63a092075b777e686ba73e88c3ec219675e0f
Glitch (hello-webpage) <none>
2020-01-11 03:51:34 +0000

    🐥🌃 Checkpoint ./memory/memory_test.js:10229510/460

--- memory/memory_test.js
+++ memory/memory_test.js
@@ -1,6 +1,21 @@
 const show_time=30;
 const test_time=30;
 
-function generate() {
+const min = 10;
+const max = 100;
+
+function generate_test() {
+  
+}
+
+function generate_numbers(seed) {
+  const rand = (s = seed) => {
+    return function() {
+        s = Math.sin(s) * 10000; return s - Math.floor(s);
+    };
+};
+
+  const randInt = () => Math.floor(Math.random() * (max - min) + min);
+
   
 }
