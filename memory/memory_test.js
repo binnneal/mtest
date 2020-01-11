7528535e51022f5cbfc61d9aa9a9a615714db04d
Glitch (hello-webpage) <none>
2020-01-11 04:02:35 +0000

    🌓🚍 Checkpoint ./memory/memory_test.js:10229510/2870

--- memory/memory_test.js
+++ memory/memory_test.js
@@ -1,21 +1,29 @@
-const show_time=30;
-const test_time=30;
+const show_time = 30;
+const test_time = 30;
 
 const min = 10;
 const max = 100;
 
+const seed = 1234;
+const rand_factory = s => {
+  return function() {
+    s = Math.sin(s) * 10000;
+    return s - Math.floor(s);
+  };
+};
+const random = rand_factory(seed);
+
 function generate_test() {
+  const numbers = generate_n umbers(len);
+  document
   
 }
 
-function generate_numbers(seed) {
-  const rand = (s = seed) => {
-    return function() {
-        s = Math.sin(s) * 10000; return s - Math.floor(s);
-    };
-};
-
-  const randInt = () => Math.floor(Math.random() * (max - min) + min);
-
-  
+function generate_numbers(len) {
+  const randInt = () => Math.floor(random() * (max - min) + min);
+  let numbers = [];
+  for (const i = 0; i < len; i++) {
+    numbers.push(randInt());
+  }
+  return numbers;
 }
