343732b5ff6adb7663a9c9fb09931e61d3922090
Yong Personal <binneal@gmail.com>
2020-10-25 22:50:07 -0700

    Added visual tests.

--- m2/memory_test.js
+++ m2/memory_test.js
@@ -1,8 +1,8 @@
 const SHOW_TIME = 20;
 const FILL_TIME = 8;
-const TEST_SIZE = 8;
+const TEST_SIZE = 7;
 const TEST_REPEAT = 2;
-const MIN = 2;
+const MIN = 10;
 const MAX = 99;
 
 const seed = 2345;
@@ -77,7 +77,6 @@ class Timer {
 class Test {
   constructor() {
     this.result = [];
-
     const numbers = new Set();
     const wrong_numbers = new Set();
     while (numbers.size < TEST_SIZE) {
@@ -95,13 +94,17 @@ class Test {
     console.log(this.wrong_numbers_list);
 
     this.numbers_d = document.getElementById("numbers_d");
-    this.input_d = document.getElementById("input_d");
-    this.buttons = input_d.querySelectorAll("input");
+    this.board_d = document.getElementById("board_d");
   }
 
+  // to be overriden
+  async show_whatever() {}
+
+  // to be overriden
+  async choose_whatever(small, big) {}
+
   async run() {
-    this.numbers_d.lastElementChild.innerHTML = this.numbers_list.join(" ");
-    await display(this.numbers_d, SHOW_TIME, 4);
+    await this.show_whatever();
 
     // show a series of multi_choice and grade answers.
     const NUM_QUESTION = TEST_SIZE;
@@ -109,8 +112,8 @@ class Test {
       this.answer = event.target.value;
       timer.expire();
     };
-    this.buttons[0].onclick = click_answer;
-    this.buttons[1].onclick = click_answer;
+    this.choices[0].onclick = click_answer;
+    this.choices[1].onclick = click_answer;
 
     while (this.result.length < NUM_QUESTION) {
       await this.do_questions();
@@ -121,10 +124,10 @@ class Test {
     this.answer = null;
     const right = this.numbers_list.shift();
     const wrong = this.wrong_numbers_list.shift();
-    this.buttons[0].value = `${right < wrong ? right : wrong}`;
-    this.buttons[1].value = `${right < wrong ? wrong : right}`;
+    const small = right < wrong ? right : wrong;
+    const big = right < wrong ? wrong : right;
+    await this.choose_whatever(small, big);
 
-    await display(input_d, FILL_TIME, 1);
     this.result.push([`${right}`, this.answer]);
     console.log(this.result);
   }
@@ -141,6 +144,92 @@ class Test {
   }
 }
 
+class TestNumbers extends Test {
+  constructor() {
+    super();
+
+    this.input_d = document.getElementById("input_d");
+    this.choices = this.input_d.querySelectorAll("input");
+  }
+
+  async choose_whatever(small, big) {
+    this.choices[0].value = `${small}`;
+    this.choices[1].value = `${big}`;
+    await display(this.input_d, FILL_TIME, 1);
+  }
+
+  async show_whatever() {
+    this.numbers_d.lastElementChild.innerHTML = this.numbers_list.join(" ");
+    await display(this.numbers_d, SHOW_TIME, 4);
+  }
+}
+
+
+class Canvas {
+  constructor(canvas) {
+    const SZ = 500;
+    this.c = canvas;
+    this.c.height = this.c.width = SZ;
+    this.ctx = canvas.getContext("2d");
+    this.do_grid();
+  }
+
+  do_grid() {
+    const SZ = this.c.height;
+    this.ctx.strokeStyle = "gray";
+    this.ctx.lineWidth = 1;
+    this.ctx.setLineDash([4, 2]);
+    for (let i = SZ / 4; i < SZ; i += SZ / 4) {
+      this.line(i, 0, i, SZ);
+    }
+    for (let i = SZ / 4; i < SZ; i += SZ / 4) {
+      this.line(0, i, SZ, i);
+    }
+  }
+
+  line(x1, y1, x2, y2) {
+    this.ctx.moveTo(x1, y1);
+    this.ctx.lineTo(x2, y2);
+    this.ctx.stroke();
+  }
+
+  fill(n, clear_first = false) {
+    if (clear_first) {
+      this.ctx.clearRect(0, 0, this.c.width, this.c.height);
+      this.do_grid();
+    }
+    this.ctx.font = "20px verdana";
+    //this.ctx.fillText(`${n}`, Math.floor(n / 10) * 50, (n % 10) * 50 + 25);
+    this.ctx.fillText(`*`, Math.floor(n / 10) * 50, (n % 10) * 50 + 25);
+    this.c.value = `${n}`;
+  }
+}
+
+class TestPatterns extends Test {
+  constructor() {
+    super();
+    this.canvas_d = document.getElementById("canvas_d");
+    this.choices = this.canvas_d.querySelectorAll("canvas");
+    this.canvas = [
+      new Canvas(this.choices[0]), new Canvas(this.choices[1])];
+  }
+
+  async show_whatever() {
+    const canvas = new Canvas(document.getElementById("board_c"));
+    for (const n of this.numbers_list) {
+      canvas.fill(n);
+    }
+    await display(this.board_d, SHOW_TIME, 4);
+  }
+
+  async choose_whatever(small, big) {
+    this.canvas[0].fill(small, true);
+    this.canvas[1].fill(big, true);
+
+    await display(this.canvas_d, FILL_TIME, 1);
+  }
+}
+
 const states = {
   START_TEST: 0,
   TEST: 1,
@@ -149,7 +238,8 @@ const states = {
 
 class TestSuite {
   constructor() {
-    console.log("TestSuite");
+    this.test_numbers = true;
+
     // reset all elements
     let content_d = document.getElementById("content_d");
     document.body.replaceChild(content_d.cloneNode(true), content_d);
@@ -159,29 +249,47 @@ class TestSuite {
     this.result_e = document.getElementById("result_d");
 
     this.master_b = document.getElementById("master_b");
-    this.master_b.value = "Click to start test set";
+    this.master_b.value = `Start ${this.type} Test`;
     this.state = states.START_TEST;
-    this.master_b.addEventListener("click", () => this.handle_state());
+    this.master_b.addEventListener("click", (e) => this.handle_state(e));
+
+    this.switch_b = document.getElementById("switch_b");
+    this.switch_b.value = `Switch To Visual`;
+    this.switch_b.addEventListener("click", (e) => this.handle_state(e));
+
     document.getElementById("subject_h1").addEventListener("click",
       () => timer.expire()
     );
   }
 
-  async handle_state() {
+  get type() {
+    return this.test_numbers ? "Number" : "Visual";
+  }
+  get other_type() {
+    return !this.test_numbers ? "Number" : "Visual";
+  }
+
+  async handle_state(e) {
+    if (e && e.target == this.switch_b) {
+      this.switch_b.value = `Switch To ${this.type}`;
+      this.test_numbers = !this.test_numbers;
+    }
     if (this.state == states.START_TEST) {
+      hide(this.switch_b);
       hide(this.result_e);
-      this.master_b.value = `Test ${this.results.length + 1} of ${TEST_REPEAT}`;
+      this.master_b.value = `${this.type} Test ${this.results.length + 1} of ${TEST_REPEAT}`;
       this.state = states.TEST;
       display(this.master_b);
     } else if (this.state == states.TEST) {
       hide(this.master_b);
-      let test = new Test();
+      let test = this.test_numbers ? new TestNumbers() : new TestPatterns();
       await test.run();
       this.get_result(test);
     } else if (this.state == states.RESULT) {
       this.master_b.value = "Restart Test";
       this.results = [];
       display(this.master_b);
+      display(this.switch_b);
       this.state = states.START_TEST;
     }
   }
